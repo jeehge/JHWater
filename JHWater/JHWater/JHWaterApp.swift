a3fcc71a92c7d0bc26c6ad83662507ff852eed44
jeehge <taerg89@gmail.com>
2023-06-24 20:47:14 +0900

    create project

---
+++ JHWater/JHWater/JHWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JHWaterApp.swift
+//  JHWater
+//
+//  Created by JH on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct JHWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JHWater/JHWaterWidget/JHWaterWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  JHWaterWidgetBundle.swift
+//  JHWaterWidget
+//
+//  Created by JH on 2023/06/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct JHWaterWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        JHWaterWidget()
+        JHWaterWidgetLiveActivity()
+    }
+}
