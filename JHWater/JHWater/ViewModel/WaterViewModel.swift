d56f7d83d9aef3b1671bcab037666b7cbf310033
jeehge <taerg89@gmail.com>
2023-07-26 02:31:06 +0900

    count 셋팅하는거 viewmodel 로 뺌

--- JHWater/JHWater/ViewModel/WaterViewModel.swift
+++ JHWater/JHWater/ViewModel/WaterViewModel.swift
@@ -7,8 +7,31 @@
 
 import Combine
 import SwiftUI
+import WidgetKit
 
 public class WaterViewModel: ObservableObject {
 	@Published var drinkCount: Int = UserDefaults.shared.integer(forKey: "drinkCount")
 	@Published var totalCount: Int = UserDefaults.shared.integer(forKey: "totalCount")
+	
+	func setTotal(count: Int) {
+		UserDefaults.standard.dictionaryRepresentation().forEach { key, value in
+			UserDefaults.shared.set(value, forKey: key)
+		}
+		UserDefaults.shared.set(count, forKey: "totalCount")
+		WidgetCenter.shared.reloadTimelines(ofKind: "JHWaterWidget")
+	}
+	
+	func setDrink(count: Int) {
+		UserDefaults.standard.dictionaryRepresentation().forEach { key, value in
+			UserDefaults.shared.set(value, forKey: key)
+		}
+		
+		if count > totalCount {
+			UserDefaults.shared.set(totalCount, forKey: "drinkCount")
+		} else {
+			UserDefaults.shared.set(count, forKey: "drinkCount")
+		}
+		
+		WidgetCenter.shared.reloadTimelines(ofKind: "JHWaterWidget")
+	}
 }
